88eaa32223a932da00a2117b199f8f59a5b93ae9
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2024-07-09 16:51:25 -1200

    next commit

---
+++ Classwork/pythontask.py
@@ -0,0 +1,7 @@
+firstName = input("Enter your first name: ")
+
+lastName = input("Enter your last name: ")
+
+age = input("Enter your age: ")
+
+print("Your fullname is ", firstName, lastName, "and your age is ", int(age))
